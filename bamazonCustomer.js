6fc77fe5ce93cc6febbb0866596c420fbca54243
handomekal <handomekal@gmail.com>
2019-04-30 20:42:34 -0400

    new changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,32 +14,76 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    console.log("connected as id " + connection.threadaId + "\n");
 
-    start();
+    makeTable()
 });
 
-function start() {
-    inquirer
-        .prompt({
-            name: "saleItems",
-            type: "list",
-            message: "To make a purchase provide [ID] and how many units you'd like ",
-            choices: ["ID", "# of UNITS"]
-        })
-        .then(function (answer) {
-            if (answer.saleItems === "ID") {
-                idProducts();
-            }
-            else if (answer.saleItems === "# of UNITS") {
-                unitProducts();
-
-            }
-            else {
-                connection.end();
-            }
-        })
+var makeTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " || " + res[i].product_name + " || " + res[i].dempartment_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
+        }
+        promptCustomer(res);
+    })
 }
 
-function idProducts() {}
-
-
+var promptCustomer = function (res) {
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "Which sneaker would you like to buy? [Press Q to quit]",
+    }]).then(function (answer) {
+        var correct = false;
+        if (answer.choice.toUpperCase() == "Q") {
+            process.exit();
+        }
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == answer.choice) {
+                correct = true;
+                var product_name = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quant",
+                    message: "How many would to like to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                })
+            }
+        }
+    })
+    
+    function start(){
+        if (isNaN(value) == false) {
+            return true;
+        }
+        else 
+        { 
+            return false;
+        }
+    }
+    
+    }.then(function (answer) {
+        if ((res[id].stock_quantity - answer.quant) > 0) {
+            connection.query("UPDATE products SET stock_quantity= " + (res[id].stock_quantity - answer.quant) + "WHERE product_name= " + product + " ' ", function (err, res2) {
+                console.log("Nice kicks!");
+                makeTable();
+            });
+        }
+        else {
+            console.log("Sorry, Not a valid selection");
+            promptCustomer(res);
+        }
+        if (i == res.length && correct == false) {
+            console.log("Sorry, Not a valid selection");
+            promptCustomer(res);
+        }
+    });
+}
